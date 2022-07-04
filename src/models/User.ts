2e0de5b03f73328b09b18a6549c0114f35c40701
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-04 20:53:23 -0300

    feat: Added user model

---
+++ src/models/User.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  profileImage: string;
+  bio: string;
+}
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profileImage: { type: String },
+    bio: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const UserModel = mongoose.model<IUser>("User", userSchema);
