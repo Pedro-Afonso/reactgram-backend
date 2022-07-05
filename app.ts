a3b9e313bf7e254404b3ab4c1b8e05d8ab5e2df7
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-05 11:41:23 -0300

    feat: Solved cors

--- app.ts
+++ app.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
+import cors, { CorsOptions } from "cors";
 // database connection
 import "./src/config/db";
 
+const port = process.env.PORT;
 const app = express();
 
-const port = process.env.PORT;
+// Config JSON and form data response
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Solve Cors
+const allowedOrigins = ["http://localhost:3000"];
+const corsOptions: CorsOptions = {
+  credentials: true,
+  origin: allowedOrigins,
+};
+app.use(cors(corsOptions));
 
 // teste route
 app.get("/", (req: Request, res: Response) => {
