9ca1f035a5d5fbefb1f20854493bd05ab51b45c3
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-05 15:58:03 -0300

    feat: Added register user function

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,18 +1,57 @@
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { UserModel } from "../models/User";
+import { UserModel, IUser } from "../models/UserModel";
 
-const jwtSecret = process.env.JWT_SECRET;
+const jwtSecret = process.env.JWT_SECRET || "asdasdasdasd";
 
 // Generate user token
 const generateToken = (id: number) => {
   return jwt.sign({ id }, jwtSecret, { expiresIn: "5d" });
 };
 
-// Register user and sign in
-const register = async (req: Request, res: Response) => {
-  res.send("User registration success!");
+interface IRegisterRequest extends Request {
+  body: IUser;
+}
+
+// Register user
+const register = async (req: IRegisterRequest, res: Response) => {
+  const { name, email, password } = req.body;
+
+  // Check if user exists
+  const user = await UserModel.findOne({ email });
+
+  if (user) {
+    res
+      .status(422)
+      .json({ errors: "Já existe um usuário cadastrado com esse e-mail." });
+    return;
+  }
+
+  // Generate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  // Create user
+  const newUser = await UserModel.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  // Check if user was created sucessfully
+  if (!newUser) {
+    res.status(422).json({
+      errors: ["Houve um erro, por favor tente mais tarde."],
+    });
+    return;
+  }
+
+  // Return the token
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
 };
 
 export { register };
