3d0ea67f2a8d79318a0ddec4641c39cfe76ade26
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-05 20:01:00 -0300

    feat: Added user validation middleware

---
+++ src/middlewares/userValidations.ts
@@ -0,0 +1,32 @@
+import { body } from "express-validator";
+
+const userCreateValidation = () => {
+  return [
+    body("name")
+      .isString()
+      .withMessage("O nome é obrigatório")
+      .isLength({ min: 3 })
+      .withMessage("O nome precisa ter no mínimo 3 caracteres"),
+    body("email")
+      .isString()
+      .withMessage("O email é obrigatório")
+      .isEmail()
+      .withMessage("Insira um e-mail válido"),
+    body("password")
+      .isString()
+      .withMessage("A senha é obrigatória.")
+      .isLength({ min: 8 })
+      .withMessage("A senha precisa de no mínimo 8 caracteres."),
+    body("confirmPassword")
+      .isString()
+      .withMessage("A confirmação de senha é obrigatória.")
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("As senhas não são iguais.");
+        }
+        return true;
+      }),
+  ];
+};
+
+export { userCreateValidation };
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -5,9 +5,10 @@ import { register } from "../controllers/UserController";
 
 // Middlewares
 import { validate } from "../middlewares/handleValidations";
+import { userCreateValidation } from "../middlewares/userValidations";
 
 const router = express();
 
-router.post("/register", validate, register);
+router.post("/register", userCreateValidation(), validate, register);
 
 export { router as UserRoutes };
