c7a042185db28e32794887da1464337e6f83de2a
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-06 15:56:01 -0300

    feat: Added update user route

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,20 +1,35 @@
 import express from "express";
 
 // Controllers
-import { register, login, getCurrentUser } from "../controllers/UserController";
+import {
+  register,
+  login,
+  getCurrentUser,
+  update,
+} from "../controllers/UserController";
 
 // Middlewares
 import { validate } from "../middlewares/handleValidations";
 import {
   loginValidation,
   userCreateValidation,
+  userUpdateValidation,
 } from "../middlewares/userValidations";
 import { authGuard } from "../middlewares/authGuard";
+import { imageUpload } from "../middlewares/imageUpload";
 
 const router = express();
 
 router.post("/register", userCreateValidation(), validate, register);
 router.get("/profile", authGuard, getCurrentUser);
 router.post("/login", loginValidation(), validate, login);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
 
 export { router as UserRoutes };
