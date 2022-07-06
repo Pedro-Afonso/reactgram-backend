cbb333663968fe39fdeb9440c6e7edda33582b80
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-06 11:21:28 -0300

    feat: Added image upload middleware

---
+++ src/middlewares/imageUpload.ts
@@ -0,0 +1,36 @@
+import path from "path";
+import multer from "multer";
+
+// Destination to store image
+const imageStorage = multer.diskStorage({
+  destination(req, _, cb) {
+    let folder = "";
+
+    if (req.baseUrl.includes("users")) {
+      folder = "users";
+    } else if (req.baseUrl.includes("photos")) {
+      folder = "photos";
+    }
+    cb(null, `upload/${folder}`);
+  },
+
+  filename: (_, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStorage,
+  fileFilter(req, file, cb) {
+    // upload only png and jpg format
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      return cb(
+        new Error("Por favor, envie apenas fotos no formato png ou jpg!")
+      );
+    }
+
+    cb(null, true);
+  },
+});
+
+export { imageUpload };
