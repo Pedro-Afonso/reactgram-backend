3927d6e7da23b22d3f3540feeac29a74bd512dde
Pedro-Afonso <pedro.766@hotmail.com>
2022-08-21 15:59:59 -0300

    Update

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+const dbUri = process.env.DB_MONGO_URI;
 
 const conn = async () => {
   try {
-    const dbConn = await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.wbryvtq.mongodb.net/?retryWrites=true&w=majority`
-    );
+    const dbConn = await mongoose.connect(dbUri);
     console.log("Database connection successful!");
     return dbConn;
   } catch (error) {
@@ -16,6 +13,4 @@ const conn = async () => {
   }
 };
 
-conn();
-
-export = { conn };
+export { conn };
