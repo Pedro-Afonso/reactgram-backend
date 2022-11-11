3c5441d23de714a33332efeaf3e2ff9bdf0a740f
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-11 10:46:54 -0300

    feat: Add type for id in generateToken

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,9 +1,10 @@
-import jwt from 'jsonwebtoken'
+import { sign } from 'jsonwebtoken'
+import { Types } from 'mongoose'
 import 'dotenv/config'
 
 const jwtSecret = process.env.JWT_SECRET
 
 // Generate user token
-export const generateToken = (id: number): string => {
-  return jwt.sign({ id }, jwtSecret, { expiresIn: '5d' })
+export const generateToken = (id: Types.ObjectId): string => {
+  return sign({ id }, jwtSecret, { expiresIn: '5d' })
 }
