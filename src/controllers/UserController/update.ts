095d7eeed0190259b45c233cbd867467d04b1c3c
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-11 12:11:32 -0300

    feat: Change findById to findOneAndUpdate

--- src/controllers/UserController/update.ts
+++ src/controllers/UserController/update.ts
@@ -4,6 +4,13 @@ import 'dotenv/config'
 import { generatePasswordHash } from '../../utils'
 import { UserModel, IUser } from '../../models/UserModel'
 
+interface IUpdateForm {
+  name?: string
+  bio?: string
+  profileImage?: string
+  password?: string
+}
+
 interface IUpdateRequest extends Request {
   body: {
     name: IUser['name']
@@ -16,36 +23,29 @@ interface IUpdateRequest extends Request {
 export const update = async (req: IUpdateRequest, res: Response) => {
   const { name, password, bio } = req.body
 
-  let profileImage = null
-
-  if (req.file) {
-    profileImage = req.file.location
-  }
-
   const reqUser = req.user
 
-  const user = await UserModel.findById(reqUser._id).select('-password')
+  const update = {} as IUpdateForm
 
   if (name) {
-    user.name = name
-  }
-  if (password) {
-    user.password = await generatePasswordHash(password)
-  }
-
-  if (profileImage) {
-    user.profileImage = profileImage
+    update.name = name
   }
 
   if (bio) {
-    if (bio === 'undefined') {
-      user.bio = ''
-    } else {
-      user.bio = bio
-    }
+    update.bio = bio
   }
 
-  await user.save()
+  if (req.file) {
+    update.profileImage = req.file.location
+  }
+
+  if (password) {
+    update.password = await generatePasswordHash(password)
+  }
+
+  const user = await UserModel.findOneAndUpdate({ _id: reqUser._id }, update, {
+    new: true
+  }).select('-password')
 
   res.status(200).json(user)
 }
