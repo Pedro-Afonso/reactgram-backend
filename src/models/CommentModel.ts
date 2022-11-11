0d4d45afa2f146a47fdd71e78cc3d8e0941a5147
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-11 10:53:24 -0300

    feat: Add photo id reference

--- src/models/CommentModel.ts
+++ src/models/CommentModel.ts
@@ -1,20 +1,24 @@
 import mongoose, { Types, Schema } from 'mongoose'
 
-interface ICommentSchema {
-  _id?: string
-  comment: string
-  user: Types.ObjectId
+interface IComment {
+  _id?: Types.ObjectId
+  text?: string
+  user?: Types.ObjectId
+  photo?: Types.ObjectId
   createdAt?: Date
   updatedAt?: Date
   __v?: number
 }
 
-const commentSchema = new Schema({
-  comment: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: 'User' }
-})
-
-export const CommentModel = mongoose.model<ICommentSchema>(
-  'Comment',
-  commentSchema
+const commentSchema = new Schema(
+  {
+    text: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    photo: { type: Schema.Types.ObjectId, ref: 'Photo' }
+  },
+  {
+    timestamps: true
+  }
 )
+
+export const CommentModel = mongoose.model<IComment>('Comment', commentSchema)
